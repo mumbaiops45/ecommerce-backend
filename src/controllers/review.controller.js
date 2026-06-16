ad7ef743e34162189623e774eb2b783d42ed770c
mumbaiops45 <mumbaiops45@gmail.com>
2026-06-16 18:44:19 +0530

    first ct

--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -32,7 +32,7 @@ export const updateReview = async (req, res) => {
 
     return res.status(200).json({
       success: true,
-      message: "Review updated successfully",
+      message: "Review updatd successfully",
       review,
     });
   } catch (err) {
