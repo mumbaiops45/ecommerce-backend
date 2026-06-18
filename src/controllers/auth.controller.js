05532b541044b347dea6365c57351621526de18e
mumbaiops45 <mumbaiops45@gmail.com>
2026-06-18 12:44:45 +0530

    17/06

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,7 +3,7 @@ import { registerUser, loginUser } from "../services/auth.service.js";
 const COOKIE_OPTIONS = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "strict",
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
